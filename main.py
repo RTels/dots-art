fb5bbd431ca32dade2e83c8dd55c9ccadb133a37
Rafael <rrft@cin.ufpe.br>
2023-09-20 09:53:39 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,61 @@
+import turtle
+from turtle import Turtle, Screen
+import random
+colors = [(0, 0, 0), (246, 245, 241), (136, 78, 52), (26, 85, 158), (218, 134, 59), (184, 18, 59), (137, 190, 3),
+          (150, 193, 205), (1, 144, 74), (222, 41, 97), (246, 145, 163), (249, 92, 13), (150, 42, 110)]
+
+turtle.colormode(255)
+timmy = turtle.Turtle()
+timmy.speed("fastest")
+timmy.penup()
+timmy.setheading(255)
+timmy.forward(300)
+timmy.setheading(0)
+
+dots_number = 100
+def change_pos():
+    timmy.setheading(90)
+    timmy.forward(50)
+    timmy.setheading(180)
+    timmy.forward(500)
+    timmy.setheading(0)
+
+
+for dot_count in range(1, dots_number+1):
+    timmy.dot(20, random.choice(colors))
+    timmy.forward(50)
+    if dot_count % 10 == 0:
+        change_pos()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
+
+
+
+
+
+
+
+
+
+
+
+
